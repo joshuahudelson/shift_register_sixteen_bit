135727b17ed122ea13c6758ae0860e91c8ed810b
Joshua Hudelson <joshua.hudelson@gmail.com>
2017-08-18 19:58:46 -0400

    Initial commit.

---
+++ polybang.c
@@ -0,0 +1,40 @@
+#include "m_pd.h"
+
+static t_class * polybang_class;
+
+typedef struct _polybang {
+  t_object    x_obj;
+  t_int       init_count, current_count;
+  t_int       mod_A, mod_B;
+}t_polybang;
+
+void polybang_setMods(t_polybang * x, t_floatarg f1, t_floatarg f2){
+  x->mod_A = (f1 <= 0) ? 1 : f1;
+  x->mod_B = (f2 <=0) ? 1: f2;
+}
+
+void polybang_resetCount(t_polybang * x){
+  x->init_count = 0;
+  x->current_count = x->init_count;
+}
+
+void * polybang_new(t_floatarg f1, t_floatarg f2){
+  t_polybang * x = (t_polybang *)pd_new(polybang_class);
+
+  polybang_resetCount(x);
+
+  polybang_setMods(x, f1, f2);
+
+  return (void *) x;
+}
+
+void polybang_setup(void){
+  polybang_class = class_new(gensym("polybang"),
+                             (t_newmethod) polybang_new,
+                             0,
+                             sizeof(t_polybang),
+                             CLASS_DEFAULT,
+                             A_DEFFLOAT,
+                             A_DEFFLOAT,
+                             0);
+}
