a96c7081a1731a2a331c0770714351af3d13401c
Joshua Hudelson <joshua.hudelson@gmail.com>
2017-08-20 09:10:26 -0400

    Added float inlets.

--- polybang.c
+++ polybang.c
@@ -6,6 +6,8 @@ typedef struct _polybang {
   t_object    x_obj;
   t_int       init_count, current_count;
   t_int       mod_A, mod_B;
+  t_outlet    * out_A, * out_B, * out_synch, * out_count;
+  t_inlet     * in_mod_A, * in_mod_B;
 }t_polybang;
 
 void polybang_setMods(t_polybang * x, t_floatarg f1, t_floatarg f2){
@@ -20,6 +22,18 @@ void polybang_resetCount(t_polybang * x){
 
 void polybang_onBangMSG(t_polybang * x){
   post("Ready to go.");
+  outlet_bang(x->out_A);
+  outlet_bang(x->out_B);
+  outlet_bang(x->out_synch);
+  outlet_float(x->out_count, 1);
+}
+
+void polybang_onSet_A(t_polybang * x, t_floatarg f){
+  polybang_setMods(x, f, x->mod_B);
+}
+
+void polybang_onSet_B(t_polybang * x, t_floatarg f){
+  polybang_setMods(x, x->mod_A, f);
 }
 
 void * polybang_new(t_floatarg f1, t_floatarg f2){
@@ -29,13 +43,31 @@ void * polybang_new(t_floatarg f1, t_floatarg f2){
 
   polybang_setMods(x, f1, f2);
 
+  x->out_A = outlet_new(&x->x_obj, &s_bang);
+  x->out_B = outlet_new(&x->x_obj, &s_bang);
+  x->out_synch = outlet_new(&x->x_obj, &s_bang);
+  x->out_count = outlet_new(&x->x_obj, &s_float);
+
+  x->in_mod_A = inlet_new(&x->x_obj, &x->x_obj.ob_pd, &s_float, gensym("ratio_A"));
+  x->in_mod_B = inlet_new(&x->x_obj, &x->x_obj.ob_pd, &s_float, gensym("ratio_B"));
+
   return (void *) x;
+}
+
+void polybang_free(t_polybang * x){
+  outlet_free(x->out_A);
+  outlet_free(x->out_B);
+  outlet_free(x->out_synch);
+  outlet_free(x->out_count);
+
+  inlet_free(x->in_mod_A);
+  inlet_free(x->in_mod_B);
 }
 
 void polybang_setup(void){
   polybang_class = class_new(gensym("polybang"),
                              (t_newmethod) polybang_new,
-                             0,
+                             (t_method)polybang_free,
                              sizeof(t_polybang),
                              CLASS_DEFAULT,
                              A_DEFFLOAT,
@@ -43,4 +75,16 @@ void polybang_setup(void){
                              0);
 
   class_addbang(polybang_class, (t_method) polybang_onBangMSG);
+
+  class_addmethod(polybang_class,
+                   (t_method) polybang_onSet_A,
+                   gensym("ratio_A"),
+                   A_DEFFLOAT,
+                   0);
+
+ class_addmethod(polybang_class,
+                  (t_method) polybang_onSet_B,
+                  gensym("ratio_B"),
+                  A_DEFFLOAT,
+                  0);
 }
