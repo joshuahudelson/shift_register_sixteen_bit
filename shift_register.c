bb2ad9e01a55a4c6c77c0cdb377263fe2752a9d6
Joshua Hudelson <joshua.hudelson@gmail.com>
2017-10-04 21:26:59 -0400

    Just changed names.

--- shift_register.c
+++ shift_register.c
@@ -1,27 +1,27 @@
 #include "m_pd.h"
 
-static t_class * ShiftReg_class
+static t_class * shift_register_class
 
-typedef Struct _ShiftReg{
+typedef Struct _shift_register{
   t_object    x_obj;
   t_int       num_elems, current_exponent;
   t_float     element_array[100];
   t_inlet     * in_elems, * in_exp;
   t_outlet    * out;
 
-}t_ShiftReg;
+}t_shift_register;
 
 
-void reset_Reg(t_ShiftReg * x){
+void reset_Reg(t_shift_register * x){
   for (i=0; i<x->num_elems; i++){
     x->element_array[i] = 0.0;
   }
 }
 
 
-void * ShiftReg_new(t_floatarg f1, t_floatarg f2){
+void * shift_register_new(t_floatarg f1, t_floatarg f2){
 
-  t_ShiftReg * x = (t_ShiftReg *) pd_new(ShiftReg_class);
+  t_shift_register * x = (t_shift_register *) pd_new(shift_register_class);
 
   reset_Reg(x);
 
@@ -30,4 +30,9 @@ void * ShiftReg_new(t_floatarg f1, t_floatarg f2){
   x->out      = outlet_new(&x->x_obj, &s_float);
 
   return (void *) x;
+}
+
+void shift_register_free(t_shift_register * x){
+
+
 }
